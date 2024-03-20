598f5f9877f45e3ab742c7b3ae5aa0206b9ecf04
jacketdembys <jacketdembys@yahoo.fr>
2024-03-19 20:18:07 -0500

    train with xval

--- docker/yolov8-job-auto-xval-del.sh
+++ docker/yolov8-job-auto-xval-del.sh
@@ -5,6 +5,6 @@ split_end=10
 split_increment=1
 
 for (( split=split_start; split<=split_end; split+=split_increment )); do
-    job_name="yolo-job-jacket-increase-augment-v8-${num_class}class-xval"
+    job_name="yolo-job-jacket-increase-augment-v8-${num_class}class-xval-${split}"
     kubectl delete job $job_name
 done
