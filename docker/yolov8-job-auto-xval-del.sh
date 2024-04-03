4c74fb7a2e0ca5c2cb3317229216ee2432bfd58c
jacketdembys <jacketdembys@yahoo.fr>
2024-04-03 00:28:42 -0500

    videowise xval

--- docker/yolov8-job-auto-xval-del.sh
+++ docker/yolov8-job-auto-xval-del.sh
@@ -6,7 +6,7 @@ split_increment=1
 
 for (( split=split_start; split<=split_end; split+=split_increment )); do
     #video_id=$(printf "%03d" $split)
-    #job_name="yolo-job-jacket-increase-augment-v8-${num_class}class-xval-${split}"
-    job_name="rtdetr-job-jacket-increase-augment-${num_class}class-xval-${split}"
+    job_name="yolo-job-jacket-increase-augment-v8-${num_class}class-xval-${split}"
+    #job_name="rtdetr-job-jacket-increase-augment-${num_class}class-xval-${split}"
     kubectl delete job $job_name
 done
